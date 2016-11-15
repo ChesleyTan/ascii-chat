b19242cf7d96a5effa3b73b059403c2657e0c1a0
Chesley Tan <chesleytan97@gmail.com>
2016-11-15 12:29:26 -0500

    Use byte sequences instead of strings to avoid realloc overhead

--- src/cv.ml
+++ src/cv.ml
@@ -12,13 +12,13 @@ let coordinate_to_index (height, width, depth) (col, row, dep) =
 let string_of_uchar = Unsigned.UChar.to_string
 let ascii_of_uchar n =
     if n < 50 then
-        "`"
+        '`'
     else if n >= 50 && n < 100 then
-        "."
+        '.'
     else if n >= 100 && n < 200 then
-        "*"
+        '*'
     else
-        "#"
+        '#'
 
 let print_unbuf s =
     Printf.printf "%s" s;
@@ -26,6 +26,9 @@ let print_unbuf s =
 
 let clear_screen () =
     print_unbuf "\x1B[2J"
+
+let restore_cursor () =
+    print_unbuf "\x1B[;H"
 
 (* C++ interface bindings *)
 let frame = foreign "read_frame" (void @-> returning (ptr uchar))
@@ -43,28 +46,33 @@ let print_frame () =
     let c2i = coordinate_to_index (height, width, depth) in
     let get_int col row dep =
         get frame_array (c2i (col, row, dep)) |> Unsigned.UChar.to_int in
-    let im = ref "\x1B[;H" in
+    (* Allocate space for each line and newline *)
+    let buf = Bytes.create (height * (width + 1)) in
+    let idx = ref 0 in
     for row = 0 to height - 1 do
         for col = 0 to width - 1 do
             let avg =
                 ((get_int col row 0) +
                  (get_int col row 1) +
                  (get_int col row 2)) / 3
-            in im := !im ^ (ascii_of_uchar avg)
+            in Bytes.set buf !idx (ascii_of_uchar avg);
             (*
             Printf.printf "(%s,%s,%s);"
                 (string_of_uchar @@ get frame_array @@ c2i (col, row, 0))
                 (string_of_uchar @@ get frame_array @@ c2i (col, row, 1))
                 (string_of_uchar @@ get frame_array @@ c2i (col, row, 2))
             *)
+            incr idx
         done;
-        im := !im ^ "\n"
+        Bytes.set buf !idx '\n';
+        incr idx
     done;
-    print_unbuf !im
+    print_unbuf buf
 
 let _ =
     clear_screen ();
     while true; do
+        restore_cursor ();
         print_frame ();
         Unix.sleepf 0.1;
     done;
