c98968c4dfeab830ae407e45f30577b398013ede
Lucas Chen <lmc336@cornell.edu>
2016-12-02 23:28:06 -0500

    Update hashtable mapping for messaging history

--- src/messaging.ml
+++ src/messaging.ml
@@ -1,21 +1,22 @@
-(*Empty history buffer stored as a list*)
+(*History buffer stored as a list*)
 let history_buffer = ref [""]
 
-(*Hashtable for keeping track of last messages for each user*)
+(* TODO: Determine size hashtable should me initialized at.
+ *Hashtable for keeping track of last messages for each user*)
 let message_mapping = Hashtbl.create 1
 
 (*Take user identity (ip address + port), a message, and a timestamp as input, and populate an internal buffer that represents the accumulated chat
  * history, which should only be updated when a new message is received*)
-let add_to_buffer user message timestamp = 
+let add_to_history_buffer user message timestamp = 
     history_buffer := !history_buffer @ [user] @ [message] @ [timestamp]; ()
 
 (*Refreshes the history buffer*)
-let refresh_buffer user message timestamp = 
+let refresh_history_buffer user message timestamp = 
     if Hashtbl.mem message_mapping user && Hashtbl.find
-        message_mapping user = message^timestamp
+        message_mapping user = timestamp
     then ()
-    else let _ = Hashtbl.replace message_mapping user (message^timestamp) in 
-        add_to_buffer user message timestamp
+    else let _ = Hashtbl.replace message_mapping user timestamp in 
+        add_to_history_buffer user message timestamp
 
 (*Calling to_string should return a string of the chat history*)
 let to_string = List.fold_left (fun accum str -> accum ^ str) "" !history_buffer  
--- src/messaging.mli
+++ src/messaging.mli
@@ -2,8 +2,8 @@ val history_buffer: string list ref
 
 (*val message_mapping = ('_a, '_b) Hashtbl.t *)
 
-val add_to_buffer: string -> string -> string -> unit
+val add_to_history_buffer: string -> string -> string -> unit
 
-val refresh_buffer: string -> string -> string -> unit
+val refresh_history_buffer: string -> string -> string -> unit
 
 val to_string: string
