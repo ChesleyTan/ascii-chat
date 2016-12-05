6c1fe793664e3d68348827a4ed2ec152634e9675
Danny Qiu <dqiu55@gmail.com>
2016-12-05 03:47:09 -0500

    Fix gossip issues with new connections

--- src/network.ml
+++ src/network.ml
@@ -2,6 +2,8 @@ open Package
 open Utils
 open Lwt
 
+let print_debug_endline = if true then print_endline else ignore
+
 let my_address = Utils.get_address_self()
 
 let my_inet_address = Unix.inet_addr_of_string my_address
@@ -14,41 +16,50 @@ let string_of_sockaddr = function
 let connections = Hashtbl.create 4
 
 let broadcast data =
-  let send_data _ oc = output_string oc data; flush oc in
+  let send_data id oc = output_string oc data; flush oc in
   Hashtbl.iter send_data connections
 
 let send package =
   let msg = package |> serialize |> encrypt in
-  print_endline @@ "Sending message: " ^ msg;
-  broadcast @@ "M" ^ (msg |> String.length |> string_of_int) ^ "\n" ^ msg
+  print_debug_endline @@ "Broadcasting message: " ^ msg;
+  broadcast @@ "M" ^ (msg |> String.length |> string_of_int) ^ "\n" ^ msg ^ "\n"
+
+let send_gossip oc =
+  let gossip = Hashtbl.fold (fun k _ s -> s ^ ";" ^ k) connections "" in
+  let gossip = "G" ^ gossip ^ "\n" in
+  print_debug_endline @@ "Sending gossip: " ^ gossip;
+  output_string oc gossip; flush oc
 
 let broadcast_gossip () =
   let gossip = Hashtbl.fold (fun k _ s -> s ^ ";" ^ k) connections "" in
-  print_endline @@ "Broadcasting gossip: " ^ gossip;
-  broadcast @@ "G" ^ gossip
+  print_debug_endline @@ "Broadcasting gossip: " ^ gossip;
+  broadcast @@ "G" ^ gossip ^ "\n"
 
 let broadcast_drop id =
-  print_endline @@ "Broadcasting drop: " ^ id;
-  broadcast @@ "D" ^ id
+  print_debug_endline @@ "Broadcasting drop: " ^ id;
+  broadcast @@ "D" ^ id ^ "\n"
 
-let rec update_connection cb c =
+let rec open_connection cb id =
+  match Str.split (Str.regexp ":") id with
+  | [addr; port] ->
+      begin
+        print_debug_endline @@ "Connecting to " ^ addr ^ ":" ^ port;
+        let open Unix in
+        let sock = socket PF_INET SOCK_STREAM 0 in
+        setsockopt sock SO_REUSEADDR true;
+        let remote_addr = ADDR_INET (Unix.inet_addr_of_string addr,
+                                     int_of_string port) in
+        connect sock remote_addr;
+        accept_connection cb false (sock, remote_addr)
+      end
+  | _ -> ()
+
+and update_connection cb c =
   if Hashtbl.mem connections c then ()
-  else
-    match Str.split (Str.regexp ":") c with
-    | [addr; port] ->
-        begin
-          print_endline @@ "Connecting to " ^ addr ^ ":" ^ port;
-          let open Unix in
-          let sock = socket PF_INET SOCK_STREAM 0 in
-          setsockopt sock SO_REUSEADDR true;
-          let remote_addr = ADDR_INET (Unix.inet_addr_of_string addr,
-                                       int_of_string port) in
-          connect sock remote_addr;
-          accept_connection cb false (sock, remote_addr)
-        end
-    | _ -> ()
+  else open_connection cb c |> ignore
 
 and handle_gossip cb gossip =
+  print_debug_endline @@ "Received gossip: " ^ gossip;
   let conns = Str.split (Str.regexp ";") gossip in
     List.iter (update_connection cb) conns
 
@@ -58,13 +69,13 @@ and handle_msg cb id ic msg_len =
       begin
         if len > 0 then
           let ciphertext = really_input_string ic len in
-          print_endline @@ "Received: " ^ ciphertext;
+          print_debug_endline @@ "Received: " ^ ciphertext;
           (* TODO: handle gracefully *)
           let package = ciphertext |> decrypt |> deserialize in
             cb id package
-        else print_endline @@ "Message length cannot be 0"
+        else print_debug_endline @@ "Message length cannot be 0"
       end
-  | None -> print_endline @@ "Message length not an integer: " ^ msg_len
+  | None -> print_debug_endline @@ "Message length not an integer: " ^ msg_len
 
 and handle_drop id =
   Hashtbl.remove connections id
@@ -78,8 +89,8 @@ and handle_line cb id ic line =
     | 'G' -> handle_gossip cb data
     | 'M' -> handle_msg cb id ic data
     | 'D' -> handle_drop data
-    | c -> print_endline @@ "Received unknown header: " ^ (String.make 1 c)
-  else print_endline @@ "Received invalid line: " ^ line
+    | c -> print_debug_endline @@ "Received unknown header: " ^ (String.make 1 c)
+  else print_debug_endline @@ "Received invalid line: " ^ line
 
 and handle_connection cb id ic () =
   try
@@ -88,25 +99,48 @@ and handle_connection cb id ic () =
   with
   | End_of_file ->
       begin
-        print_endline @@ "Connection dropped: " ^ id;
+        print_debug_endline @@ "Connection dropped: " ^ id;
         Hashtbl.remove connections id
       end
 
-and accept_connection cb new_client (fd, remote_addr) =
-  let id = string_of_sockaddr remote_addr in
+and handle_identity oc line =
+  let line = String.trim line in
+  if String.length line > 0 then
+    let id = String.sub line 1 (String.length line - 1) in
+    match String.get line 0 with
+    | 'I' -> Hashtbl.add connections id oc; id
+    | c -> ""
+  else ""
+
+and shutdown_connection fd ic oc =
+  let open Unix in
+  shutdown fd SHUTDOWN_ALL;
+  close_in_noerr ic;
+  close_out_noerr oc;
+  close fd
+
+and accept_connection cb new_client (fd, _) =
   let ic = Unix.in_channel_of_descr fd in
   let oc = Unix.out_channel_of_descr fd in
-  print_endline @@ "New connection: " ^ id;
-  Hashtbl.add connections id oc;
-  if new_client then
-    broadcast_gossip ()
-  ;
-  Lwt_preemptive.detach (fun () ->
-    handle_connection cb id ic ()
-  ) () |> ignore
+  let my_id = my_address ^ ":" ^ (string_of_int port) in
+  output_string oc ("I" ^ my_id ^ "\n"); flush oc;
+  let id = input_line ic |> handle_identity oc in
+  if id = "" then shutdown_connection fd ic oc
+  else
+    print_debug_endline @@ "New connection: " ^ id;
+    Hashtbl.add connections id oc;
+    if new_client then broadcast_gossip ();
+    send_gossip oc;
+    Lwt_preemptive.detach (fun () ->
+      handle_connection cb id ic ()
+    ) () |> ignore
 
 (* Adapted from: http://baturin.org/code/lwt-counter-server/ *)
 let network_initialize port cb host_addr =
+  let dev_null = open_out "/dev/null" in
+  let my_id = my_address ^ ":" ^ (string_of_int port) in
+  Hashtbl.add connections my_id dev_null;
+  if host_addr <> "" then open_connection cb host_addr;
   Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
   let open Unix in
   let sock = socket PF_INET SOCK_STREAM 0 in
