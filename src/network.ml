18ffc8b4256ad90404f3de8fe3f84fd28a05aa07
Danny Qiu <dqiu55@gmail.com>
2016-12-04 22:13:17 -0500

    Fix module issue

--- src/network.ml
+++ src/network.ml
@@ -1,8 +1,9 @@
 open Package
 open Unix
+open Utils
 open Lwt
 
-let my_address = get_address_self()
+let my_address = Utils.get_address_self()
 
 let my_inet_address = Unix.inet_addr_of_string my_address
 
